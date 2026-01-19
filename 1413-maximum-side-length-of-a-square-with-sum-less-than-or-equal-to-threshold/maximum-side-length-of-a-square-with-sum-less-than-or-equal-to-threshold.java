96440670c3755df2dde3f58cfcd59135698b16be
Kush <Kushbohare@gmail.com>
2026-01-19 22:29:08 +0530

    Time: 8 ms (45.69%) | Memory: 57.8 MB (27.92%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,38 +1,99 @@
-class Solution {
-    boolean isValid(int row,int col,int side,int x,int mat[][],int rowSum[][]){
-        int sum=0;
+// class Solution {
+//     boolean isValid(int row,int col,int side,int x,int mat[][],int rowSum[][]){
+//         int sum=0;
        
         
-        for(int i=row;i < row +side;i++){
-            sum += rowSum[i][col + side -1] - (col > 0 ? rowSum[i][col - 1] : 0);
+//         for(int i=row;i < row +side;i++){
+//             sum += rowSum[i][col + side -1] - (col > 0 ? rowSum[i][col - 1] : 0);
            
-            if(sum > x) return false;
-        }
-        return true;
+//             if(sum > x) return false;
+//         }
+//         return true;
        
         
+//     }
+//     public int maxSideLength(int[][] mat, int threshold) {
+//         int m=mat.length;
+//         int n=mat[0].length;
+
+//         int rowSum[][]=new int[m][n];
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 int prefix=0;
+//                 if(j > 0) prefix=rowSum[i][j-1];
+//                 rowSum[i][j]=prefix + mat[i][j];
+//             }
+//         }
+
+//         int low=0;
+//         int high=Math.min(m,n);
+//         while(low <= high){
+//             int side=(low + high)>>1;
+//             for(int i=0;i + side<= m  ;i++){
+//                 for(int j=0;j + side <=n;j++){
+//                     if(isValid(i,j,side,threshold,mat,rowSum)) return side;
+//                 }
+//             }
+//         }
+
+//         return 0;
+//     }
+// }
+
+class Solution {
+
+    public int maxSideLength(int[][] matrix, int threshold) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] prefixSum = new int[rows + 1][cols + 1];
+
+        // build 2D prefix sum
+        for (int r = 1; r <= rows; r++) {
+            for (int c = 1; c <= cols; c++) {
+                prefixSum[r][c] =
+                        matrix[r - 1][c - 1]
+                        + prefixSum[r - 1][c]
+                        + prefixSum[r][c - 1]
+                        - prefixSum[r - 1][c - 1];
+            }
+        }
+
+        int left = 0;
+        int right = Math.min(rows, cols);
+        int answer = 0;
+
+        while (left <= right) {
+            int sideLength = left + (right - left) / 2;
+
+            if (squareExists(prefixSum, sideLength, threshold, rows, cols)) {
+                answer = sideLength;
+                left = sideLength + 1;
+            } else {
+                right = sideLength - 1;
+            }
+        }
+
+        return answer;
     }
-    public int maxSideLength(int[][] mat, int threshold) {
-        int m=mat.length;
-        int n=mat[0].length;
 
-        int rowSum[][]=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int prefix=0;
-                if(j > 0) prefix=rowSum[i][j-1];
-                rowSum[i][j]=prefix + mat[i][j];
+    private boolean squareExists(int[][] prefixSum,
+                                 int sideLength,
+                                 int threshold,
+                                 int rows,
+                                 int cols) {
+
+        for (int r = sideLength; r <= rows; r++) {
+            for (int c = sideLength; c <= cols; c++) {
+                int squareSum =
+                        prefixSum[r][c]
+                        - prefixSum[r - sideLength][c]
+                        - prefixSum[r][c - sideLength]
+                        + prefixSum[r - sideLength][c - sideLength];
+
+                if (squareSum <= threshold) return true;
             }
         }
-
-        for(int side=Math.min(m,n);side>=1;side--){
-            for(int i=0;i + side<= m  ;i++){
-                for(int j=0;j + side <=n;j++){
-                    if(isValid(i,j,side,threshold,mat,rowSum)) return side;
-                }
-            }
-        }
-
-        return 0;
+        return false;
     }
 }
