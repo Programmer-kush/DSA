db0975013e6c432ebfde61ac4e5047c7d202c931
Kush <Kushbohare@gmail.com>
2026-01-21 15:51:16 +0530

    Time: 30 ms (5.56%) | Memory: 46.9 MB (55.56%) - LeetSync

---
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    int solve(int num){
+        int n=num;
+        int pos=0;
+        if(num==2) return -1;
+        while(pos < 64){
+            if((n & 1)==1 && ((n>>1) & 1)==0){
+                break;
+            }
+            n=n>>1;
+            pos++;
+        }
+        System.out.println("pos for " + num + " is " + pos);
+
+        return num ^ (1 << pos);
+    }
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n=nums.size();
+        int ans[]=new int[n];
+        for(int i=0;i<n;i++){
+            ans[i]=solve(nums.get(i));
+        }
+
+        return ans;
+    }
+}
+
+//01 from left to right pe flip the 1
