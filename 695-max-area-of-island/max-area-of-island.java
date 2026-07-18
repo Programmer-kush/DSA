234127c1fb690cc242590ef7b493e65d2657bcd5
Kush <Kushbohare@gmail.com>
2026-07-18 09:16:31 +0530

    Time: 2 ms (63.94%) | Memory: 46.6 MB (51.23%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,37 +1,32 @@
 class Solution {
-    int dx[]={0,0,1,-1};
-    int dy[]={1,-1,0,0};
-    public void dfs(int row,int col,int [][]grid,boolean visited[][],int count[]){
-        visited[row][col]=true;
-        count[0]++;
-        for(int k=0;k<4;k++){
-            int nRow=row+dx[k];
-            int nCol=col + dy[k];
+    int[] dx = {0, 0, 1, -1};
+    int[] dy = {1, -1, 0, 0};
 
-            if(nRow >=0 && nRow < grid.length && nCol>=0 && nCol < grid[0].length){
-                if(!visited[nRow][nCol] && grid[nRow][nCol]==1) dfs(nRow,nCol,grid,visited,count);
-            }
+    public int dfs(int row, int col, int[][] grid) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == 0)
+            return 0;
+
+        grid[row][col] = 0;
+
+        int area = 1;
+        for (int k = 0; k < 4; k++) {
+            area += dfs(row + dx[k], col + dy[k], grid);
         }
+
+        return area;
     }
+
     public int maxAreaOfIsland(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
+        int ans = 0;
 
-        boolean visited[][]=new boolean[m][n];
-        int count[]={0};
-        int ans=0;
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!visited[i][j] && grid[i][j]==1){
-                    dfs(i,j,grid,visited,count);
-                    ans=Math.max(ans,count[0]);
-                    count[0]=0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    ans = Math.max(ans, dfs(i, j, grid));
                 }
             }
         }
 
         return ans;
-        
     }
 }
