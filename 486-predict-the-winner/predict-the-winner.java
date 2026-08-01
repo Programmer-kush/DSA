d85dd1aa497e78a038898a251fc3f8bd1374975d
Kush <Kushbohare@gmail.com>
2026-08-01 11:55:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (41.06%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solve(int i,int j,int nums[],int dp[][]){
+        if(i > j) return 0;
+
+        if(dp[i][j] !=-1) return dp[i][j];
+
+        int first=nums[i]  + Math.min(solve(i+2,j,nums,dp),solve(i+1,j-1,nums,dp));
+        int last=nums[j] +  Math.min(solve(i+1,j-1,nums,dp),solve(i,j-2,nums,dp)); 
+
+        return dp[i][j]=Math.max(first,last);
+    }
+    public boolean predictTheWinner(int[] nums) {
+        int n=nums.length;
+        int dp[][]=new int[n][n];
+        for(int arr[] : dp){
+            Arrays.fill(arr,-1);
+        }
+        int player1=solve(0,n-1,nums,dp);
+        int total=0;
+        for(int num : nums) total += num;
+        if(total%2==0){
+            if(player1 == total/2) return true;
+        }
+            
+        
+        return (player1 > total/2);
+    }
+}
