53409498bce772287bec9c167007fead21b27cfc
Kush <Kushbohare@gmail.com>
2026-08-02 10:39:16 +0530

    Time: 14 ms (5.73%) | Memory: 46.2 MB (22.27%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -14,8 +14,24 @@ class Solution {
     public boolean stoneGame(int[] piles) {
         int n=piles.length;
         int dp[][]=new int[n][n];
-        for(int arr[] : dp) Arrays.fill(arr,-1);
-        int alice=solve(0,n-1,piles,dp);
+        //for(int arr[] : dp) Arrays.fill(arr,-1);
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                int a=0;
+                if(i+1 < n && j-1 >=0 )a=dp[i+1][j-1];
+                int b=0;
+                if(i+2< n)b=dp[i+2][j];
+                int c=0;
+                if(j-2 >=0) c=dp[i][j-2];
+                int first=piles[i] + Math.min(a,b);
+                int last=piles[j] + Math.min(a,c);
+
+                dp[i][j]=Math.max(first,last);
+            }
+        }
+
+        int alice=dp[0][n-1];
         int sum=0;
         for(int num : piles) sum+=num;
         return (alice > sum-alice);
