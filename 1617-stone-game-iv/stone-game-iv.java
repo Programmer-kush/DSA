7ca3ccbbc6ad482c332649b51298c851c37997d3
Kush <Kushbohare@gmail.com>
2026-08-11 12:05:10 +0530

    Time: 155 ms (8.14%) | Memory: 43.1 MB (56.43%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -11,8 +11,18 @@ class Solution {
     }
     public boolean winnerSquareGame(int n) {
         Boolean dp[]=new Boolean[n+1];
-
-        return solve(n,dp);
+        for(int j=1;j<=n;j++){
+            for(int i=1;i*i <= j;i++){
+                boolean opp=solve(j - i*i,dp);
+                if(opp==false){
+                  dp[j]=true;
+                }
+                    
+            }
+             if(dp[j]==null)dp[j]=false;
+        }
+       // return solve(n,dp);
+        return dp[n];
 
     }
 }
